3697dec257cbac83a702846a6fde68deacabcd9c
pgrandne <perrin.grandne@protonmail.com>
2021-05-15 16:31:30 +0200

    Auth page added

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { NavLink } from  'react-router-dom';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = props => {
+    const auth = useContext(AuthContext);
+
     return (
         <ul className="nav-links">
             <li>
                 <NavLink to="/" exact>Tous les utilisateurs</NavLink>
             </li>
-            <li>
-                <NavLink to="/u1/places">Mes places</NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new">Ajouter une place</NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth">Se connecter</NavLink>
-            </li>
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/u1/places">Mes places</NavLink>
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/places/new">Ajouter une place</NavLink>
+                </li>
+            )}
+            {!auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/auth">Se connecter</NavLink>
+                </li>
+            )}
+                        {auth.isLoggedIn && (
+                <li>
+                    <button onClick={auth.logout}>Se déconnecter</button>
+                </li>
+            )}
         </ul>
     )
 };
